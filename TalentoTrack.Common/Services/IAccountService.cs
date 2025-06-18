05fb54fbc740fcbf757e3f2cb69d1aa15f68b2a1
SudamBaikare <sudambaikare143@gmail.com>
2025-06-18 11:44:33 +0530

    -initial commit -Implemented base architecture -Created Login web API

---
+++ TalentoTrack.Common/Services/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalentoTrack.Common.DTOs.Account;
+
+namespace TalentoTrack.Common.Services
+{
+    public interface IAccountService
+    {
+        Task<LoginResponse> VerifyLoginDetails(LoginRequests requests);
+
+    }
+}
---
+++ TalentoTrack.Service/AccountService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalentoTrack.Common.DTOs.Account;
+using TalentoTrack.Common.Repositories;
+using TalentoTrack.Common.Services;
+
+namespace TalentoTrack.Service
+{
+    public class AccountService : IAccountService
+    {
+
+        public readonly IAccountRepository _accountRepository;
+
+        public AccountService(IAccountRepository accountRepository)
+        {
+
+            _accountRepository = accountRepository;
+        }
+       public async Task<LoginResponse> VerifyLoginDetails(LoginRequests requests)
+        {
+            LoginResponse response = new LoginResponse();
+            try
+            {
+                var dbUser =await _accountRepository.GetLoginDetails(requests.Username!, requests.Password!);
+                if (dbUser == null) { 
+                response.Succes=false;
+                response.ErrorMessage = "Invalid Credentials";
+                }
+                else
+                {
+                    response.Succes=true;
+                }
+                return response;
+            }
+            catch(Exception e) {
+                throw;
+            }
+
+        }
+    }
+}
